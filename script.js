0b8578fec97cbfbf42a3762b3e2ff19d83625716
Edwin Reynoso <eorroe@gmail.com>
2015-07-09 21:27:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+(function() {
+	var elementSetters = {}, elementGetters = {}, changedNodes = [];
+	for(var prop in HTMLElement.prototype) {
+		try {
+			HTMLElement.prototype[prop];
+		} catch(e) {
+			elementGetters[prop] = HTMLElement.prototype.__lookupGetter__(prop);
+			elementSetters[prop] = HTMLElement.prototype.__lookupSetter__(prop);
+			(function() {
+				var prop2 = prop;
+				Object.defineProperty(HTMLElement.prototype, prop2, {
+					get() {
+						if(this["_" + prop2] !== undefined) {
+							return this["_" + prop2];
+						} else {
+							return elementGetters[prop2].call(this);
+						}
+					},
+					set(newVal) {
+						this["_" + prop2] = newVal;
+						changedNodes.push({node: this, prop: prop2, newVal});
+					}
+				});
+			})();
+		}
+	}
+
+	document.rerender = function() {
+		var change;
+		while(change = changedNodes.shift()) {
+			elementSetters[change.prop].call(change.node, change.newVal);
+			delete change.node["_" + change.prop];
+		}
+	}
+})();
