ed37e031da798c7faf7a0c2c19a9a319753ebc41
Edwin Reynoso <eorroe@gmail.com>
2015-08-30 14:45:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 (function() {
 	var elementSetters = {}, elementGetters = {}, changes = [];
-	for(var prop in HTMLElement.prototype) {
+	function magic(prop) {
 		try {
-			HTMLElement.prototype[prop];
+			var oldFunc = HTMLElement.prototype[prop];
+			if(oldFunc instanceof Function) {
+				HTMLElement.prototype[prop] = function() {
+					changes.push( { node: this, prop: prop, func: oldFunc, args: arguments} );
+				}
+			}
 		} catch(e) {
 			elementGetters[prop] = HTMLElement.prototype.__lookupGetter__(prop);
 			elementSetters[prop] = HTMLElement.prototype.__lookupSetter__(prop);
-			(function() {
-				var prop2 = prop;
-				Object.defineProperty(HTMLElement.prototype, prop2, {
-					get() {
-						if(this["_" + prop2] !== undefined) {
-							return this["_" + prop2];
-						} else {
-							return elementGetters[prop2].call(this);
-						}
-					},
-					set(newVal) {
-						this["_" + prop2] = newVal;
-						changes.push({node: this, prop: prop2, newVal});
+			var propObj = { value: null }
+			Object.defineProperty(HTMLElement.prototype, prop, {
+				get: function() {
+					if(propObj.value !== null) {
+						return propObj.value;
+					} else {
+						return elementGetters[prop].call(this);
 					}
-				});
-			})();
+				},
+				set: function(newVal) {
+					propObj.value = newVal;
+					changes.push( { node: this, prop: prop, newVal: newVal, propObj: propObj } );
+				}
+			});
 		}
 	}
 
-	document.rerender = function() {
+	for(var prop in HTMLElement.prototype) magic(prop);
+
+	document.updateDOM = function() {
 		var change;
 		while(change = changes.shift()) {
-			elementSetters[change.prop].call(change.node, change.newVal);
-			delete change.node["_" + change.prop];
+			if(change.node[change.prop] instanceof Function) {
+				change.func.apply(change.node, change.args);
+			} else {
+				elementSetters[change.prop].call(change.node, change.newVal);
+				change.propObj.value = null;
+			}
 		}
 	}
 })();
