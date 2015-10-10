5df69edc2ca7bb007a585b404024ac35f6c4d00f
Edwin Reynoso <eorroe@gmail.com>
2015-10-10 07:42:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 (function() {
-	var elementSetters = {}, elementGetters = {}, methods = {}, changes = []
+	var elementSetters = {}, elementGetters = {}, methods = {}, changes = [];
 	function magic(prop) {
-		try {
-			methods[prop] = HTMLElement.prototype[prop];
-			HTMLElement.prototype[prop] = function() {
-				var change = { prop: prop, func: methods[prop], node: this, args: arguments };
-				return new Promise(function(res) {
+		if( div[ prop ] instanceof Function ) {
+			methods[ prop ] = div[ prop ];
+			HTMLElement.prototype[ prop ] = function() {
+				var change = { prop: prop, func: methods[ prop ], node: this, args: arguments };
+				return new Promise( function( res ) {
 					change.res = res;
-					changes.push(change);
+					changes.push( change );
 				});
 			}
-		} catch(e) {
-			elementGetters[prop] = HTMLElement.prototype.__lookupGetter__(prop);
-			elementSetters[prop] = HTMLElement.prototype.__lookupSetter__(prop);
+		} else {
+			elementGetters[ prop ] = div.__lookupGetter__( prop );
+			elementSetters[ prop ] = div.__lookupSetter__( prop );
 			var change = { prop: prop, value: null }
-			Object.defineProperty(HTMLElement.prototype, prop, {
+			Object.defineProperty( HTMLElement.prototype, prop, {
 				get: function() {
-					if(change.value !== null) {
+					if( change.value !== null ) {
 						return change.value;
 					} else {
-						return change.value = elementGetters[prop].call(this);
+						return change.value = elementGetters[ prop ].call( this );
 					}
 				},
-				set: function(newVal) {
-					if(elementSetters[prop] !== undefined) {
+				set: function( newVal ) {
+					if( elementSetters[ prop ] !== undefined ) {
 						change.node = this, change.value = newVal;
 						for(var i = 0, l = changes.length; i < l; i++) {
-							var c = changes[i];
-							if(c.node === this && c.prop === prop) return;
+							var c = changes[ i ];
+							if( c.node === this && c.prop === prop ) return;
 						}
-						changes.push(change);
+						changes.push( change );
 					}
 				}
 			});
 		}
 	}
 
-	for(var prop in HTMLElement.prototype) magic(prop);
+	var div = document.createElement( 'div' );
+	for( var prop in div ) magic( prop );
+	div = prop = null;
 
 	document.updateDOM = function() {
 		var change;
-		while(change = changes.shift()) {
-			if(change.node[change.prop] instanceof Function) {
-				change.res( change.func.apply(change.node, change.args) );
+		while( change = changes.shift() ) {
+			if( change.node[ change.prop ] instanceof Function ) {
+				change.res( change.func.apply( change.node, change.args ) );
 			} else {
-				elementSetters[change.prop].call(change.node, change.value);
+				elementSetters[ change.prop ].call( change.node, change.value );
 				change.value = null;
 			}
 		}
